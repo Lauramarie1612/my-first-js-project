ba3000ae06c4df299e076ffdcd52401b4ba71b33
Lauramarie1612 <eaa25lmbj@students.eaaa.dk>
2025-09-24 13:37:14 +0200

    ændringer

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-document.body.innerHTML += "Hello JavaScript!";
-alert("Hello Dat.js");
-console.log(Hello);
+document.body.innerHTML += "welcome";
+
+//alert("Hello Dat.js");
+console.log("Hello Dat.js");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
